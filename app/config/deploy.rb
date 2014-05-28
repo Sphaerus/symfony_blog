04d4e4f82a69ecd1350e4e001232a8a0a02eca85
Sphaerus <wyfsm1@gmail.com>
2014-05-28 15:31:51 +0200

    deploy.rb

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,17 +1,19 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+set :application, "blog"
+set :domain,      "12_polaczek@wierzba.wzks.uj.edu.pl"
+set :deploy_to,   "/public_html/blog.pl"
+set :app_path,    "blog.pl"
+default_run_options[:pty] = true
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "/Users/Kuba/Desktop/php/blog.git"
+set :deploy_via, :rsync_with_remote_cache
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
 # Or: `propel`
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :web,        "12_polaczek@wierzba.wzks.uj.edu.pl"                         # Your HTTP server, Apache/etc
+role :app,        "12_polaczek@wierzba.wzks.uj.edu.pl", :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  3
 
